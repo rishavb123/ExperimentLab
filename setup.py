67eceecfb5b4bdac010897ee88f4116478f6f10e
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-02-24 14:26:07 -0500

    more initial setup

--- setup.py
+++ setup.py
@@ -1,12 +1,37 @@
+import pathlib
+
 from setuptools import setup
 
-import glob
+
+CWD = pathlib.Path(__file__).absolute().parent
+
+
+def get_version():
+    """Gets the gymnasium version."""
+    path = CWD / "gymnasium" / "__init__.py"
+    content = path.read_text()
+
+    for line in content.splitlines():
+        if line.startswith("__version__"):
+            return line.strip().split()[-1].strip().strip('"')
+    raise RuntimeError("bad version data in __init__.py")
+
+
+def get_description():
+    """Gets the description from the readme."""
+    with open("README.md") as fh:
+        long_description = ""
+        header_count = 0
+        for line in fh:
+            if line.startswith("##"):
+                header_count += 1
+            if header_count < 2:
+                long_description += line
+            else:
+                break
+    return long_description
 
 setup(
-    name='experiment_lab',
-    version='0.0.1',
-    keywords='experiment, suite, lab',
-    url='https://github.com/rishavb123/ExperimentLab',
-    description='A novelty experimentation wrapper for minigrid',
-    packages=['experiment_lab'],
+    name="experiment_lab",
+    url="https://github.com/rishavb123/ExperimentLab",
 )
