c38d430d7615f6ec489eea02003af95ffc1adb9f
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-03-03 18:18:32 -0500

    config changes

--- experiment_lab/experiments/rl/config.py
+++ experiment_lab/experiments/rl/config.py
@@ -12,19 +12,23 @@ from experiment_lab.core.base_config import BaseConfig
 
 @dataclass
 class WandbCallbackConfig:
+    """The wandb callback config dataclass."""
+
     verbose: int = 2
-    model_save_path: str = "./models"
     model_save_freq: int = 1_000
     gradient_save_freq: int = 0
 
 
 class RLConfig(BaseConfig):
-    env_config: Dict[str, Any]
+    """The RL Config dataclass."""
+
+    env_config: Dict[str, Any] = {"env_id": "CartPole-v1"}
+    transfer_steps: List[int] | None = None
 
     total_time_steps: int = 10_000
     n_envs: int = 1
 
-    wrappers: List[gym.Wrapper] | None = None
+    wrappers: List[Type[gym.Wrapper]] | None = None
     wrapper_kwargs_lst: List[Dict[str, Any]] | None = None
 
     model_cls: Type[BaseAlgorithm] = PPO
@@ -33,9 +37,19 @@ class RLConfig(BaseConfig):
     policy_cls: Type[BasePolicy] = ActorCriticPolicy
     policy_kwargs: Dict[str, Any] | None = None
 
-    callbacks: List[BaseCallback] | None = None
+    callback_cls_lst: List[Type[BaseCallback]] | None = None
     callback_kwargs_lst: List[Dict[str, Any]] | None = None
     wandb_callback_kwargs: WandbCallbackConfig | None = None
+
+    monitor_dir: str | None = None
+    monitor_kwargs: Dict[str, Any] | None = None
+
+    start_method: str | None = None
+    render_mode: str | None = None
+
+    log: bool = True
+    log_interval: int = 1
+    save_model: bool = False
 
     device: str = "mps"
     gpu_idx: int | None = None
@@ -49,11 +63,11 @@ class RLConfig(BaseConfig):
         for _ in range(len(self.wrapper_kwargs_lst), len(self.wrappers)):
             self.wrapper_kwargs_lst.append({})
 
-        if self.callbacks is None:
-            self.callbacks = []
+        if self.callback_cls_lst is None:
+            self.callback_cls_lst = []
         if self.callback_kwargs_lst is None:
             self.callback_kwargs_lst = []
-        for _ in range(len(self.callback_kwargs_lst), len(self.callbacks)):
+        for _ in range(len(self.callback_kwargs_lst), len(self.callback_cls_lst)):
             self.callback_kwargs_lst.append({})
 
         if self.model_kwargs is None:
@@ -61,7 +75,14 @@ class RLConfig(BaseConfig):
         if self.policy_kwargs is None:
             self.policy_kwargs = {}
 
+        if self.monitor_kwargs is None:
+            self.monitor_kwargs = {}
+
+        if self.transfer_steps is None:
+            self.transfer_steps = []
+
 
 def register_configs():
+    """Registers the rl config."""
     cs = ConfigStore.instance()
     cs.store(name="rl_config", node=RLConfig)
