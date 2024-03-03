5badc9d5134e9b8dd63352586280805078645528
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-03-03 18:20:00 -0500

    switched to logger

--- experiment_lab/experiments/examples/random_waits.py
+++ experiment_lab/experiments/examples/random_waits.py
@@ -1,11 +1,13 @@
 import time
 import numpy as np
 import wandb
+import logging
 
 from experiment_lab.core.base_config import BaseConfig
 from experiment_lab.core.base_experiment import BaseExperiment
 from experiment_lab.core.runner import run_experiment
 
+logger = logging.getLogger(__name__)
 
 class RandomWaits(BaseExperiment):
 
@@ -14,13 +16,13 @@ class RandomWaits(BaseExperiment):
         super().__init__(cfg)
 
     def single_run(self, run_id: str = "", seed: int | None = None) -> None:
-        print("Starting")
+        logger.info("Starting")
         rng = np.random.default_rng(seed=seed)
         num = rng.integers(10)
         if self.use_wandb:
             wandb.log({"num": num})
         time.sleep(rng.integers(num))
-        print("Done!")
+        logger.info("Done!")
 
 
 if __name__ == "__main__":
