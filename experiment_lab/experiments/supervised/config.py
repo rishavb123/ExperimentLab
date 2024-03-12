be39d45a758ad616d48a7d0e6d82992c39216628
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-03-12 00:34:04 -0400

    working on supervised config

--- experiment_lab/experiments/supervised/config.py
+++ experiment_lab/experiments/supervised/config.py
@@ -1,6 +1,7 @@
 """Python file containing the configs for the supervised learning experiments."""
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Any, Dict
 from hydra.core.config_store import ConfigStore
 
 from experiment_lab.core.base_config import BaseConfig
@@ -10,11 +11,51 @@ from experiment_lab.core.base_config import BaseConfig
 class SupervisedConfig(BaseConfig):
     """The Supervised dataclass."""
 
+    dataset: Dict[str, Any] = field(
+        default_factory=lambda: {
+            "_target_": "torch.utils.data.ConcatDataset",
+            "datasets": [
+                {
+                    "_target_": "torchvision.datasets.MNIST",
+                    "root": "./data/supervised/mnist",
+                    "download": True,
+                    "train": True,
+                    "transform": {"_target_": "torchvision.transforms.ToTensor"},
+                },
+                {
+                    "_target_": "torchvision.datasets.MNIST",
+                    "root": "./data/supervised/mnist",
+                    "download": True,
+                    "train": False,
+                    "transform": {"_target_": "torchvision.transforms.ToTensor"},
+                },
+            ],
+        }
+    )
+
     train_ratio: float = 0.8
     val_ratio: float = 0.1
     test_ratio: float = 0.1
 
-    batch_size: int | None = 64
+    data_loader_kwargs: Dict[str, Any] | None = None
+
+    model: Dict[str, Any] = field(
+        default_factory=lambda: {
+            "_target_": "torch.nn.Sequential",
+            "_args_": [
+                {
+                    "_target_": "torch.nn.Flatten",
+                },
+                {
+                    "_target_": "experiment_lab.common.networks.create_mlp_network",
+                    "layer_sizes": [784, 500, 100, 10],
+                },
+            ],
+        }
+    )
+
+    device: str = "mps"
+    gpu_idx: int | None = None
 
     def __post_init__(self) -> None:
         """Does additional checks on the loaded config."""
