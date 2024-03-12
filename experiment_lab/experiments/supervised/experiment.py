c86d27f0fce0d3abba938f2cc26ab9fd3aec397b
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-03-12 00:34:22 -0400

    working on supervised experimetn

--- experiment_lab/experiments/supervised/experiment.py
+++ experiment_lab/experiments/supervised/experiment.py
@@ -1,6 +1,12 @@
 """The supervised learning experiment file."""
 
-from typing import Any
+from typing import Any, Dict
+import os
+import hydra
+import torch
+import torch.utils.data
+
+from experiment_lab.common.utils import default
 from experiment_lab.core.base_experiment import BaseExperiment
 from experiment_lab.experiments.supervised.config import SupervisedConfig
 
@@ -17,6 +23,14 @@ class SupervisedExperiment(BaseExperiment):
         """Initializes the supervised experiment config."""
         super().initialize_experiment()
 
+        self.dataset: torch.utils.data.Dataset = hydra.utils.instantiate(
+            self.cfg.dataset
+        )
+        self.device: torch.device = torch.device(self.cfg.device)
+        self.data_loader_kwargs: Dict[str, Any] = default(
+            self.cfg.data_loader_kwargs, {}
+        )
+
     def single_run(
         self, run_id: str, run_output_path: str, seed: int | None = None
     ) -> Any:
@@ -30,4 +44,32 @@ class SupervisedExperiment(BaseExperiment):
         Returns:
             Any: The post processed results.
         """
-        pass
+        # Setup the device to train on
+        if self.cfg.gpu_idx is not None:
+            os.environ["CUDA_VISIBLE_DEVICES"] = str(self.cfg.gpu_idx)
+
+        if seed is not None:
+            torch.manual_seed(seed=seed)
+
+        train_dataset, validation_dataset, test_dataset = torch.utils.data.random_split(
+            dataset=self.dataset,
+            lengths=[self.cfg.train_ratio, self.cfg.val_ratio, self.cfg.test_ratio],
+        )
+
+        train_dataloader, validation_dataloader, test_dataloader = (
+            torch.utils.data.DataLoader(
+                dataset=train_dataset, **self.data_loader_kwargs
+            ),
+            torch.utils.data.DataLoader(
+                dataset=validation_dataset, **self.data_loader_kwargs
+            ),
+            torch.utils.data.DataLoader(
+                dataset=test_dataset, **self.data_loader_kwargs
+            ),
+        )
+
+        model = hydra.utils.instantiate(self.cfg.model)
+
+        import pdb
+
+        pdb.set_trace()
